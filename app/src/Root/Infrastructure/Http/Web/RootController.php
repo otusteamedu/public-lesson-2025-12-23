30369cf38b39d8179dded29492ecbb44cbfc4363
Sergey Petrov <cl@coders-lair.com>
2025-12-23 16:57:52 +0300

    fixes commit

--- app/src/Root/Infrastructure/Http/Web/RootController.php
+++ app/src/Root/Infrastructure/Http/Web/RootController.php
@@ -5,13 +5,24 @@ namespace App\Root\Infrastructure\Http\Web;
 use CodersLairDev\ClFw\Http\Response\Trait\ResponseTrait;
 use CodersLairDev\ClFw\Routing\Attribute\AsController;
 use CodersLairDev\ClFw\Routing\Attribute\AsRoute;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 #[AsController]
 class RootController
 {
     use ResponseTrait;
+
+    private LoggerInterface $logger;
+
+    public function __construct()
+    {
+        $this->logger = new Logger('sha1_log');
+        $this->logger->pushHandler(new StreamHandler('/app/var/log/sha1_log.log'));
+    }
 
     #[AsRoute(path: '/')]
     public function rootIndex(): ResponseInterface
@@ -24,6 +35,27 @@ class RootController
         return $this->createResponse(
             psr17Factory: new Psr17Factory(),
             content: json_encode($data),
+            status: 200
+        );
+    }
+
+    #[AsRoute(path: '/getSha1Hash')]
+    public function getSha1Hash():ResponseInterface
+    {
+        $requestId = uniqid();
+        $requestSha1Hash = sha1($requestId);
+
+        $this->logger->info('getSha1Hash', [
+            'requestId' => $requestId,
+            'requestSha1Hash' => $requestSha1Hash,
+        ]);
+
+        return $this->createResponse(
+            psr17Factory: new Psr17Factory(),
+            content: json_encode([
+                'requestId' => $requestId,
+                'requestSha1Hash' => $requestSha1Hash,
+            ]),
             status: 200
         );
     }
